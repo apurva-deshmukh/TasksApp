1822001f9889f40754f6e037cbb43cbbfc6019ee
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-11 14:15:12 -0400

    Set up TableView

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+/*
+    - To show list of current to-do list items
+    - To enter new to-do list items
+    - To show previously entered to-do list items
+ 
+    - Item
+    - Date
+ */
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+class TaskItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
 }
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    
+    private var data = [TaskItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    // Mark: Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Open the screen where we can see item info and delete
+    }
+}
